2b8b876b02a1166b1fad9f1afdfe34646e0c61a4
penpen09 <ar-el.mbl.10@outlook.jp>
2020-07-21 00:59:17 +0900

    index作成及びshow作成

--- app/controllers/mytweets_controller.rb
+++ app/controllers/mytweets_controller.rb
@@ -1,5 +1,6 @@
 class MytweetsController < ApplicationController
   def index
+    @mytweets = Mytweet.all
   end
   def new
     @mytweet = Mytweet.new
@@ -7,5 +8,12 @@ class MytweetsController < ApplicationController
   def create
     Mytweet.create(content: params[:mytweet][:content])
     redirect_to new_mytweet_path
+  end
+  def show
+    @mytweet =Mytweet.find(params[:id])
+  end
+  private
+  def mytweet_params
+    params.require(:mytweet).permit(:content)
   end
 end
